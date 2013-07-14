de30f6e879607343126b582c47ece1fbd201c405
Renier van Rooyen <vanrooyen.renier@gmail.com>
2013-07-14 14:20:43 +0000

    Fixed web.js to read index.html

--- web.js
+++ web.js
@@ -1,29 +1,24 @@
 var express = require('express');
 
-var fs = require('fs');
-
 var app = express.createServer(express.logger());
 
 app.get('/',function(request, response){
- 
-var contents = fs.readFile('index.html','utf8',function(err, data){
-    if(err){
-    response.send('unable to load index.html');
-    }
 
-    else{
-	response.send(data);
-    }
+var fs = require('fs');
+
+var buf = new Buffer();
+
+response.send(buf.toString('utc-8',fs.readFileSync("index.html")));
+
+
 });
 
-var buf = new Buffer(256);
-
-var len = buff.write('\u00d + \u00bc = \u00be', 0);
-
-app.get('/', function(request, response) {
-
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
 
