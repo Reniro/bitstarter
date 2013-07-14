0c93128a926297f74a14159e311c6aa2f070b0c7
Renier van Rooyen <vanrooyen.renier@gmail.com>
2013-07-14 14:08:37 +0000

    Mod web.js to read index.html

--- web.js
+++ web.js
@@ -1,12 +1,29 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/',function(request, response){
+ 
+var contents = fs.readFile('index.html','utf8',function(err, data){
+    if(err){
+    response.send('unable to load index.html');
+    }
+
+    else{
+	response.send(data);
+    }
 });
+
+var buf = new Buffer(256);
+
+var len = buff.write('\u00d + \u00bc = \u00be', 0);
+
+app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
